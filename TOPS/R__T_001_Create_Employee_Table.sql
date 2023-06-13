10d95091726402011ca625b9a59de54c25d5b8a3
Suresh Tallapelli <Suresh.Tallapelli@kla-tencor.com>
2023-06-13 14:15:33 +0530

    added new file

--- TOPS/R__T_001_Create_Employee_Table.sql
+++ TOPS/R__T_001_Create_Employee_Table.sql
@@ -1,6 +1,6 @@
-CREATE  or REPLACE TABLE DEMO_DB.CICD.EMPLOYEE
-ID autoincrement(1,1),
+CREATE OR REPLACE TABLE DEMO_DB.CICD.EMPLOYEE
+(ID autoincrement(1,1),
 First_Name Varchar(20),
 Last_Name Varchar(20),
-Gender Varchar(2);
+Gender Varchar(2));
 
