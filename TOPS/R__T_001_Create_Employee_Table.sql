be9c44e0db6b407d3463e046919ac7c91f1aa6cd
Suresh Tallapelli <Suresh.Tallapelli@kla-tencor.com>
2023-06-13 11:52:41 +0530

    added schema name

--- TOPS/R__T_001_Create_Employee_Table.sql
+++ TOPS/R__T_001_Create_Employee_Table.sql
@@ -1,4 +1,4 @@
-CREATE  or REPLACE TABLE EMPLOYEE
+CREATE  or REPLACE TABLE DEMO_DB.CICD.EMPLOYEE
 ID autoincrement(1,1),
 First_Name Varchar(20),
 Last_Name Varchar(20),
--- TOPS/R__T_002_Create_Employee_Address.sql
+++ TOPS/R__T_002_Create_Employee_Address.sql
@@ -1,4 +1,4 @@
-CREATE  or REPLACE TABLE EMPLOYEEAddress
+CREATE  or REPLACE TABLE DEMO_DB.CICD.EMPLOYEEAddress
 ID autoincrement(1,1),
 City Varchar(100),
 State Vrachar(100);
