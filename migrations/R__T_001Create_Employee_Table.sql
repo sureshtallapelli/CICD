6c30aa564bcc8fe69a06aa3709ad8510d43e1659
Suresh Tallapelli <Suresh.Tallapelli@kla-tencor.com>
2023-06-13 12:15:04 +0530

    added new files

---
+++ migrations/R__T_001Create_Employee_Table.sql
@@ -0,0 +1,6 @@
+CREATE  or REPLACE TABLE DEMO_DB.CICD.EMPLOYEE (
+ID int,
+First_Name Varchar(20),
+Last_Name Varchar(20),
+Gender Varchar(2));
+
--- migrations/V1.1.5__Create_Employee_Table.sql
+++
@@ -1,6 +0,0 @@
-CREATE  or REPLACE TABLE DEMO_DB.CICD.EMPLOYEE
-ID int,
-First_Name Varchar(20),
-Last_Name Varchar(20),
-Gender Varchar(2);
-
