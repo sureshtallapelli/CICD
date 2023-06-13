6b1caf2d6b2fc34b036880d3c87bf33d539e1c2d
Suresh Tallapelli <Suresh.Tallapelli@kla-tencor.com>
2023-06-13 12:02:32 +0530

    modified scripts

--- migrations/R__T001_Create_Employee_Table.sql
+++ migrations/R__T001_Create_Employee_Table.sql
@@ -1,5 +1,5 @@
 CREATE  or REPLACE TABLE DEMO_DB.CICD.EMPLOYEE
-ID autoincrement(1,1),
+ID int,
 First_Name Varchar(20),
 Last_Name Varchar(20),
 Gender Varchar(2);
--- migrations/R__T002_Create_Employee_Address.sql
+++ migrations/R__T002_Create_Employee_Address.sql
@@ -1,5 +1,5 @@
 CREATE  or REPLACE TABLE DEMO_DB.CICD.EMPLOYEEAddress
-ID autoincrement(1,1),
+ID int,
 City Varchar(100),
 State Vrachar(100);
 
